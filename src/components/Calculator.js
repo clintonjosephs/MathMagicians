c9ea2422fa0f5e5c3b78548a61350bdc69e2ec2b
clintonjosephs <clintonjosephs26@gmail.com>
2022-02-07 20:00:00 +0100

    set up calculator componenent plus styling

---
+++ src/components/Calculator.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        <div className="calculator">
+          <div className="screen">0</div>
+          <button type="button">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button type="button" className="orange">+</button>
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button" className="orange">x</button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button" className="orange">-</button>
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button type="button" className="orange">+</button>
+          <button type="button" className="zero">0</button>
+          <button type="button">.</button>
+          <button type="button" className="orange">=</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
